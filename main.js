5b6e8db7120c03177db7fc635e69a55af79aee46
kalinjo <67753345+kalinjo@users.noreply.github.com>
2020-09-12 01:38:27 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
+function semafor(){
 var boje = document.getElementsByClassName('boje');
-boje[0].style.background = 'red';
-boje[1].style.background = 'gray';
-boje[2].style.background = 'gray';
+  boje[0].style.background = 'red';
+  boje[1].style.background = 'gray';
+  boje[2].style.background = 'gray';
 
-function changeToYellow() {
-  boje[1].style.background = 'Yellow';
-}
-function changeToGreen(){
+  function changeToYellow() {
+    boje[1].style.background = 'Yellow';
+  }
+  function changeToGreen(){
+      boje[0].style.background = 'gray';
+      boje[1].style.background = 'gray';
+      boje[2].style.background = 'green';
+  }
+  function changeToRed(){
     boje[0].style.background = 'gray';
+    boje[1].style.background = 'Yellow';
+    boje[2].style.background = 'gray';
+  }
+  function backToRed(){
+    boje[0].style.background = 'red';
     boje[1].style.background = 'gray';
-    boje[2].style.background = 'green';
+    boje[2].style.background = 'gray';
+  }
+  var x = setTimeout(changeToYellow,3000);
+  var y = setTimeout(changeToGreen,5000);
+  var start = setInterval(semafor,8000);
 }
-var x = setTimeout(changeToYellow,3000);
-var y = setTimeout(changeToGreen,5000);
+semafor();
